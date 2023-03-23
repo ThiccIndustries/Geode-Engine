8c3364e62f699cc8e06971f30b6f70478724bae0
coleco <aknelson100@gmail.com>
2023-03-22 19:53:42 -0500

    Update input
    Even if computer mice have more than seven buttons, GLFW only supports seven. I'm pretty sure anything above seven is virtual keyboard firmware anyhow. I also added scroll support.

--- g_input.cpp
+++ g_input.cpp
@@ -7,7 +7,7 @@
 #include "geode.h"
 
 bool i_k_keys[GLFW_KEY_LAST + 1];
-bool i_m_keys[GLFW_KEY_LAST + 1]; //GLFW_MOUSE_BUTTON_LAST is only 7, plenty of mice have more than that
+bool i_m_keys[GLFW_MOUSE_BUTTON_LAST + 1];
 
 int i_k_actions[GLFW_KEY_LAST + 1];
 int i_m_actions[GLFW_KEY_LAST + 1];
@@ -16,6 +16,11 @@ int i_k_tick_actions[GLFW_KEY_LAST + 1];
 int i_m_tick_actions[GLFW_KEY_LAST + 1];
 
 Coord2d i_m_pos{0, 0};
+Coord2d i_m_scroll{0, 0};
+bool i_m_scrollup=false;
+bool i_m_scrolldown=false;
+bool i_m_scrollleft=false;
+bool i_m_scrollright=false;
 
 void input_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
     i_k_keys[key]           = action != GLFW_RELEASE;
@@ -33,11 +38,31 @@ void input_mouse_position_callback(GLFWwindow* window, double xpos, double ypos)
     i_m_pos.x = xpos;
     i_m_pos.y = ypos;
 }
+void input_mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+    //Precision offsets
+    i_m_scroll.x = xoffset;
+    i_m_scroll.y = yoffset;
+
+    //Tests for typical use
+    //Horizontal scrolling
+    if(xoffset<0){
+		i_m_scrollleft=true;
+    }else if(xoffset>0){
+		i_m_scrollright=true;
+    }
+    //Vertical scrolling
+    if(yoffset<0){
+		i_m_scrollup=true;
+    }else if(yoffset>0){
+		i_m_scrolldown=true;
+    }
+}
 
 void input_register_callbacks(GLFWwindow* window){
     glfwSetKeyCallback(window, input_key_callback);
     glfwSetMouseButtonCallback(window, input_mouse_button_callback);
     glfwSetCursorPosCallback(window, input_mouse_position_callback);
+    glfwSetScrollCallback(window, input_mouse_scroll_callback);
 }
 
 bool input_get_key(int keycode)         { return i_k_keys[keycode]; }
@@ -60,6 +85,14 @@ void input_poll_input(){
         i_m_actions[i] = -1;
         i_k_actions[i] = -1;
     }
+    //Reset scroll offsets
+    i_m_scroll.x = 0;
+    i_m_scroll.y = 0;
+    //Reset scroll bools
+	bool i_m_scrollup=false;
+	bool i_m_scrolldown=false;
+	bool i_m_scrollleft=false;
+	bool i_m_scrollright=false;
 
     glfwPollEvents();
 }
